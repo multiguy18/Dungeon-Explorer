fcdfc8a2b2b5a6ce884beb1bef3856e11699eb70
multiguy18 <anselm1793@hotmail.com>
2018-11-25 20:51:11 +0100

    Klasse Objekt erstellt und implementiert

---
+++ DungeonExplorer/Objekte/Objekt.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonExplorer.Objekte
+{
+    public abstract class Objekt
+    {
+        protected short _posOben;
+        protected short _posLinks;
+        protected short _bezeichnung;
+
+
+        public Objekt(short posOben, short posLinks)
+        {
+            _posOben = posOben;
+            _posLinks = _posLinks;
+        }
+
+        public virtual bool WirdKollidieren(Objekt anderes)
+        {
+            return true;
+        }
+    }
+}
