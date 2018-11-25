a48878a1a3313a0c3e9564eab0ec81cdc2b0851d
multiguy18 <anselm1793@hotmail.com>
2018-11-25 20:41:05 +0100

    Zeicherklasse hinzugefügt, Textausgabe implementiert

---
+++ DungeonExplorer/Zeichner.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonExplorer
+{
+    public static class Zeichner
+    {
+        public static void Zeichne()
+        {
+            
+        }
+
+        public static void Textausgabe(string text)
+        {
+            StringBuilder empty = new StringBuilder();
+            empty.Insert(0, " ", 45);
+
+            Console.SetCursorPosition(2, 20);
+            Console.Write(empty);
+
+            Console.SetCursorPosition(2, 20);
+            Console.Write(text.Take(45).ToArray());
+
+            Console.SetCursorPosition(0, 0);
+        }
+    }
+}
