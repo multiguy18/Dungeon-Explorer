c3944883d57f9f307c96960fef173d86a042ed48
multiguy18 <anselm1793@hotmail.com>
2018-12-06 13:30:07 +0100

    Testprojekt hinzugefügt, Erster Unittest hinzugefügt

---
+++ DungeonExplorer.Test/SpielfigurTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DungeonExplorer.Objekte;
+using NUnit.Framework;
+
+namespace DungeonExplorer.Test
+{
+    [TestFixture]
+    public class SpielfigurTest
+    {
+        [Test]
+        public void SpielerNachObenBewegen()
+        {
+            short _testPosOben = 9;
+            short _testPosLinks = 10;
+
+            Spielfigur _spielfigur = new Spielfigur("Ralph", 10, 10);
+            _spielfigur.Bewege(Richtung.Oben);
+
+            Assert.IsTrue(_spielfigur.PosOben == _testPosOben && _spielfigur.PosLinks == _testPosLinks);
+        }
+    }
+}
