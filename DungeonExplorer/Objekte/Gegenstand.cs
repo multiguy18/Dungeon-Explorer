5a89104285411b8c7a171e69ae0081158fdeba92
multiguy18 <anselm1793@hotmail.com>
2019-01-04 18:03:32 +0100

    Klasse Gegenstand hinzugefügt

---
+++ DungeonExplorer/Objekte/Gegenstand.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonExplorer.Objekte
+{
+    public class Gegenstand : Objekt
+    {
+        Spielfigur spieler;
+
+        public Gegenstand(short posOben, short posLinks) : base(posOben, posLinks)
+        {
+
+        }
+
+        public void Benutze()
+        {
+
+        }
+    }
+}
