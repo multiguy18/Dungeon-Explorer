ab1a47f27d563e790a47a112545e12946c658f52
multiguy18 <anselm1793@hotmail.com>
2019-01-06 17:59:39 +0100

    Rüstung erweitert, Trank hinzugefügt

---
+++ DungeonExplorer/Objekte/Rüstung.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonExplorer.Objekte
+{
+    public abstract class Rüstung : Gegenstand
+    {
+        protected ushort _wert;
+        protected ushort _blockierterSchaden;
+
+        public ushort Wert {
+            get { return _wert; }
+        }
+
+        public Rüstung(short posOben, short posLinks) : base(posOben, posLinks)
+        {
+        }
+
+        public override void Benutze()
+        {
+        }
+
+        //TODO: Logik schadensblockierung
+        /*
+        public ushort Blockiere(ushort schaden)
+        {
+            if (schaden > _wert)
+            {
+                return _wert;
+            }
+            else if (schaden > _blockierterSchaden)
+            {
+                return _blockierterSchaden;
+            }
+        }
+        */
+    }
+}
---
+++ DungeonExplorer/Objekte/Trank.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonExplorer.Objekte
+{
+    public class Trank : Verbrauchsgegenstand
+    {
+        private sbyte _heilung;
+        private short _angriffsschaden;
+        private ushort _ep;
+
+        public Trank(short posOben, short posLinks, sbyte heilung, short angriffsschaden, ushort EP) : base(posOben, posLinks)
+        {
+            _slots = "ijk";
+            _bezeichnung = "Trank";
+            _heilung = heilung;
+            _angriffsschaden = angriffsschaden;
+            _ep = EP;
+        }
+
+        public override void Benutze()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
