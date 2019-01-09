fb9f21af403ee6a50f62c9f51d926b72140d0765
multiguy18 <anselm1793@hotmail.com>
2019-01-09 19:51:02 +0100

    Basisklasse Rüstung fertiggestellt

--- DungeonExplorer/Objekte/Ruestung.cs
+++ DungeonExplorer/Objekte/Ruestung.cs
@@ -9,7 +9,7 @@ namespace DungeonExplorer.Objekte
     public abstract class Ruestung : Gegenstand
     {
         protected ushort _wert;
-        protected ushort _blockierterSchaden;
+        protected ushort _schadensabwehr;
 
         public ushort Wert {
             get { return _wert; }
@@ -23,19 +23,33 @@ namespace DungeonExplorer.Objekte
         {
         }
 
-        //TODO: Logik schadensblockierung
-        /*
+        /// <summary>
+        /// Berechnet die Höhe der blockierten Schadenspunkte durch eine Rüstung
+        /// </summary>
+        /// <param name="schaden">Der angerichtete Schaden durch einen gegnerischen Angriff</param>
+        /// <returns></returns>
         public ushort Blockiere(ushort schaden)
         {
-            if (schaden > _wert)
+            ushort blockierterSchaden;
+
+            if (schaden > _schadensabwehr)
             {
-                return _wert;
+                blockierterSchaden = _schadensabwehr;
             }
-            else if (schaden > _blockierterSchaden)
+            else
             {
-                return _blockierterSchaden;
+                blockierterSchaden = schaden;
             }
+
+            if (blockierterSchaden > _wert)
+            {
+                blockierterSchaden = _wert;
+            }
+
+            _wert -= blockierterSchaden;
+
+            return blockierterSchaden;
         }
-        */
+        
     }
 }
